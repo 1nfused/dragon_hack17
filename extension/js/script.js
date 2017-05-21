903e8825ce82c88e32a804766930af40df6e0964
1nfused <luka.golinar@gmail.com>
2017-05-21 08:42:38 +0200

    Changes.

--- extension/js/script.js
+++ extension/js/script.js
@@ -1,39 +1,63 @@
 var SERVER = "https://thawing-basin-96551.herokuapp.com/"
 var socket = io(SERVER);
 
-socket.on('foo', function () {
-  console.log("data acq");
+// Accept emoji
+socket.on('foo', function (data) {
   floating({
-    content: "😇",
-    number: 3,
-    duration: 11
+    content: data["emoji"],
+    number: 1,
+    duration: 8
   });
 });
 
+// Accept statistical data
+socket.on('delete_acq', function(data) {
+	console.log(data);
+});
 
-function startPresentation() {
+
+function execPresentation(phase) {
+
+	// Set countdown date
   	var countDownDate = new Date().getTime();
+  	var x;
 
-  	// Update the count down every 1 second
-  	var x = setInterval(function() {
+  	if (phase == "start"){
+  		// Update the count down every 1 second
+	  	x = setInterval(function() {
+	    	// Get todays date and time
+	    	var now = new Date().getTime();
 
-    	// Get todays date and time
-    	var now = new Date().getTime();
+	    	// Find the distance between now an the count down date
+	    	var distance = countDownDate + now;
 
-    	// Find the distance between now an the count down date
-    	var distance = countDownDate + now;
+	    	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	    	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    	// Display the result in the element with id="demo"
-    	document.getElementById("#evt_timer").innerHTML = minutes + "m " + seconds + "s ";
-  	}, 1000);
+	    	// Display the result in the element with id="demo"
+	    	document.getElementById("evt_timer").innerHTML = minutes + "m " + seconds + "s ";
+	  	}, 1000);
+  	}
+  	if (phase == "stop") {
+		clearInterval(x);
+		document.getElementById("evt_timer").innerHTML = "";
+		return;
+	}
 }
 
 document.addEventListener('click', function (e) {
-	console.log("Clicked start");
-	startPresentation();
+	inner_text = document.getElementById("start_button").innerHTML;
+	if (inner_text == "START") {
+		document.getElementById("start_button").innerHTML = "STOP";
+		// Start clock
+		execPresentation("start");
+	} else {
+		document.getElementById("start_button").innerHTML = "START";
+		execPresentation("stop");
+		// Send delete message to the client
+		socket.emit('delete');
+	}
+	
 });
 
 
